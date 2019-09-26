5a160df4a317b62ea99cc509692dd9fb53dbc8ab
mtkerbeR <mtkerber@gmail.com>
2019-09-26 16:04:48 +0200

    First version - one simple function

---
+++ setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='pythonazuredevops',
+      version='0.1dev',
+      packages=['pythonazuredevops'])
+
+#see: https://python-packaging.readthedocs.io/en/latest/minimal.html
